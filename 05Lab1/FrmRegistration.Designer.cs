2d570796862363b6e16e238e880a0bd9b2174baa
jankiel09 <ezekielambat@gmail.com>
2025-10-15 19:11:21 +0800

    UI frmRegistration

---
+++ 05Lab1/FrmRegistration.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace _05Lab1
+{
+    partial class FrmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRegistration));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label8 = new Label();
+            comboBox1 = new ComboBox();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(16, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(221, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Registration";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(85, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Student No.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(151, 156);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(32, 85);
+            label4.Name = "label4";
+            label4.Size = new Size(98, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Student No.";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(32, 135);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 21);
+            label5.TabIndex = 4;
+            label5.Text = "Last Name";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(78, 185);
+            label6.Name = "label6";
+            label6.Size = new Size(40, 21);
+            label6.TabIndex = 5;
+            label6.Text = "Age";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(47, 241);
+            label7.Name = "label7";
+            label7.Size = new Size(71, 21);
+            label7.TabIndex = 6;
+            label7.Text = "Birthday";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(125, 243);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(141, 86);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(184, 23);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(141, 136);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(184, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(141, 186);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(184, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(358, 85);
+            label8.Name = "label8";
+            label8.Size = new Size(73, 21);
+            label8.TabIndex = 11;
+            label8.Text = "Program";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(446, 84);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(251, 23);
+            comboBox1.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(358, 185);
+            label9.Name = "label9";
+            label9.Size = new Size(64, 21);
+            label9.TabIndex = 13;
+            label9.Text = "Gender";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(358, 244);
+            label10.Name = "label10";
+            label10.Size = new Size(97, 21);
+            label10.TabIndex = 14;
+            label10.Text = "Contact No.";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(603, 138);
+            label11.Name = "label11";
+            label11.Size = new Size(34, 21);
+            label11.TabIndex = 15;
+            label11.Text = "M.I";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.White;
+            label12.Location = new Point(357, 137);
+            label12.Name = "label12";
+            label12.Size = new Size(88, 21);
+            label12.TabIndex = 16;
+            label12.Text = "First Name";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(446, 138);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(142, 23);
+            textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(646, 140);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(51, 23);
+            textBox5.TabIndex = 18;
+            // 
+            // FrmRegistration
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(comboBox1);
+            Controls.Add(label8);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FrmRegistration";
+            Text = "FrmRegistration";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label8;
+        private ComboBox comboBox1;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private TextBox textBox4;
+        private TextBox textBox5;
+    }
+}
