ecd9165e1f9f4f88a33463e00be63bdf03a8bf19
jankiel09 <ezekielambat@gmail.com>
2025-10-20 18:44:33 +0800

    Add DisplayToList method

--- 05Lab1/FrmOpenFile.cs
+++ 05Lab1/FrmOpenFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,26 @@ namespace _05Lab1
         {
 
         }
+
+        public void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+                    lvShowText.Items.Add(_getText);
+                }
+            }
+        }
+
     }
 }
+       
